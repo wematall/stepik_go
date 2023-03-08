9622a15237fb5bb9e294f1ed1a836a4acac1a34b
plex <plex@onion.localdomain>
2023-03-08 08:18:47 +0300

    loops third example

--- 1. Intro_to_Go_basics/1.10/lesson/lesson.go
+++ 1. Intro_to_Go_basics/1.10/lesson/lesson.go
@@ -4,16 +4,28 @@ import "fmt"
 
 func main() {
 	// first example
-	sum := 0
-	for i := 1; i < 10; i++ {
-		sum += 1
+	{
+		sum := 0
+		for i := 1; i < 10; i++ {
+			sum += 1
+		}
+		fmt.Println(sum)
 	}
-	fmt.Println(sum)
 
 	// second example
-	var i = 1
-	for ; i < 10; i++ {
-		fmt.Println(i * i)
+	{
+		var i = 1
+		for ; i < 10; i++ {
+			fmt.Println(i * i)
+		}
+	}
+	// third example
+	{
+		var i = 1
+		for i < 10 {
+			fmt.Println(i * i)
+			i++
+		}
 	}
 
 }
